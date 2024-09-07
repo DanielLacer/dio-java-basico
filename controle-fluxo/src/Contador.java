396945a17af1b0c01ce8e91ff6c90c1334fa145e
DanielLFSouza <daniel_lf_souza@estudante.sc.senai.br>
2024-09-07 01:29:25 -0300

    Entrega da atividade da DIO "controle-fluxo"

--- controle-fluxo/src/Contador.java
+++ controle-fluxo/src/Contador.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner terminal  = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número");
+        int parametroUm = terminal.nextInt();
+ 
+        System.out.println("Digite o segundo número");
+        int parametroDois = terminal.nextInt();
+
+        try {
+
+			contar(parametroUm, parametroDois);	
+            
+		}catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+
     }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+		if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+
+		int contagem = parametroDois - parametroUm;
+
+        for(int x=1; x <= contagem; x++){
+            System.out.println("Imprimindo o número "+x);
+        }
+	}
 }
